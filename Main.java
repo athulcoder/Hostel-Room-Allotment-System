8f4f263bda527cfa97e1daaa2846353a5616b0d0
athulcoder <athul.sin.cos.tan@gmail.com>
2025-07-16 07:27:00 +0530

    frame

--- Main.java
+++ Main.java
@@ -6,7 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("AI Powered Hostel Room Allotment System");
+        panel.add(label);
+
         frame.setVisible(true);
+        frame.add(panel);
 
         System.out.println("Hello world !!!");
     }
