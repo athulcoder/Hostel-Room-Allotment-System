d7988afa617ec0b9fd99df76598e1132f810bff0
athulcoder <athul.sin.cos.tan@gmail.com>
2025-10-06 17:57:40 +0530

    Panel based ui creation step 1

--- src/ui/MainUI.java
+++ src/ui/MainUI.java
@@ -4,6 +4,23 @@ import ui.screen.LoginScreen;
 import javax.swing.*;
 import java.awt.*;
 
-public class MainUI extends JFrame {
-    
+public class MainUI extends JFrame{
+
+    CardLayout cardLayout ;
+    JPanel mainPanel ;
+    MainUI(){
+
+        setTitle("Hostel Room Allotment System");
+        setSize(1000,750);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+
+
+
+
+    }
 }
