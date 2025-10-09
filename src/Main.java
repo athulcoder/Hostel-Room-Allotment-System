e3f78a2d24dd46a75e117e062eeba7fe0e12ad7c
athulcoder <athul.sin.cos.tan@gmail.com>
2025-10-09 22:19:29 +0530

    MainUI class update

--- src/Main.java
+++ src/Main.java
@@ -13,19 +13,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        RoomDAO roomDAO = new RoomDAO();
-        Room room = new Room("527","2 sharing",5,2,"H001");
-        room.setOccupancy(2);
-        roomDAO.updateRoom(room);
-        Room r = roomDAO.getRoomDetails("527");
-
-
-        for(Room re : roomDAO.getAvailableRooms()){
-
-                System.out.println(re.getRoomNumber());
-        }
-
-        new MainUI();
+//        RoomDAO roomDAO = new RoomDAO();
+//        Room room = new Room("527","2 sharing",5,2,"H001");
+//        room.setOccupancy(2);
+//        roomDAO.updateRoom(room);
+//        Room r = roomDAO.getRoomDetails("527");
+//
+//
+//        for(Room re : roomDAO.getAvailableRooms()){
+//
+//                System.out.println(re.getRoomNumber());
+//        }
+//
+//        new MainUI();
 
     }
 
